c9720183acd697b1c7aec10804328fa4f2a5594a
mandipf <mandip.farmahan@gmail.com>
2023-05-25 18:13:05 +0100

    Updates folder structure
    Original task4 folder removed and task4_2 renamed as task4 as changes captured under version control

--- task4/data_cleaning_scripts/candy_data_cleaning.R
+++ task4/data_cleaning_scripts/candy_data_cleaning.R
@@ -1,209 +1,168 @@
+# import libraries
 library(tidyverse)
+
+# source R scripts
+source(here::here("data_cleaning_scripts/R scripts/functions.R"))
+source(here::here("data_cleaning_scripts/R scripts/lookup_candy_alt_names.R"))
+source(here::here("data_cleaning_scripts/R scripts/lookup_candy_approved_names.R"))
+source(here::here("data_cleaning_scripts/R scripts/lookup_countries_alt_names.R"))
 
 
 
 #####
-# SECTION 1: FUNCTION DEFINITIONS
+# SECTION 1: IMPORT FILES
+# Rename personal details to short/tidy names
+# Remove unwanted columns within the personal details columns
 
-# function to import dirty data and perform general clean of column names
-import_xlsx_and_clean_names <- function(file_name) {
-  df <- readxl::read_xlsx(here::here(str_c("raw_data/", file_name)),
-                          col_types = "text")
+candy_2015 <- import_xlsx_and_clean_names("boing-boing-candy-2015.xlsx") %>% 
+  rename(age = `How old are you?`,
+         going_trick_or_treating = `Are you going actually going trick or treating yourself?`) %>% 
+  select(-Timestamp)
   
-  df_clean <- df %>% 
-    mutate(year = str_extract(file_name, "20[:digit:]{2}"),
-           tibble::rowid_to_column(df, "rater_id"),
-           rater_id = str_c(year, "_", rater_id),
-           .before = everything()
-    ) %>% 
-    janitor::clean_names()
-}
+candy_2016 <- import_xlsx_and_clean_names("boing-boing-candy-2016.xlsx") %>% 
+  rename(age = `How old are you?`,
+         going_trick_or_treating = `Are you going actually going trick or treating yourself?`,
+         gender = `Your gender:`,
+         country = `Which country do you live in?`) %>% 
+  select(-Timestamp)
 
-# function to convert other candy bar column to usable candy and rating data
-others_to_candy <- function(df, start_column, end_column) {
-  df %>% 
-    pivot_longer(cols = all_of(start_column):all_of(end_column),
-                 names_to = "rating",
-                 values_to = "candy"
-    ) %>%
-    separate_longer_delim(cols = candy,
-                          delim = regex("[\\.\\,\\;]")
-    ) %>% 
-    filter(!is.na(candy) & candy != "")
-}
+candy_2017 <- import_xlsx_and_clean_names("boing-boing-candy-2017.xlsx") %>% 
+  rename(age = `Q3: AGE`,
+         going_trick_or_treating = `Q1: GOING OUT?`,
+         gender = `Q2: GENDER`,
+         country = `Q4: COUNTRY`) %>% 
+  select(-`Internal ID`)
+
+
+
+#####
+# SECTION 2: MAIN QUESTIONNAIRE WITH CANDY NAMED COLUMNS STANDARDISED
+# select required columns and convert candy data to long format
+
+candy_2015_main <- candy_2015 %>%
+  select(year:going_trick_or_treating,
+         `[Butterfinger]`:`[York Peppermint Patties]`,
+         `[Necco Wafers]`) %>% 
+  pivot_longer(cols = `[Butterfinger]`:last_col(),
+               names_to = "candy",
+               values_to = "rating",
+               values_drop_na = TRUE)
+
+candy_2016_main <- candy_2016 %>%
+  select(year:country,
+         `[100 Grand Bar]`:`[York Peppermint Patties]`) %>% 
+  pivot_longer(cols = `[100 Grand Bar]`:last_col(),
+               names_to = "candy",
+               values_to = "rating",
+               values_drop_na = TRUE)
+
+candy_2017_main <- candy_2017 %>%
+  select(year:country,
+         `Q6 | 100 Grand Bar`:`Q6 | York Peppermint Patties`) %>% 
+  pivot_longer(cols = `Q6 | 100 Grand Bar`:last_col(),
+               names_to = "candy",
+               values_to = "rating",
+               values_drop_na = TRUE)  
+
+# combine all three data sets
+candy_combined <- bind_rows(candy_2015_main, candy_2016_main,
+                            candy_2017_main) %>% 
+  # remove wrapper characters from candy column and format rating column
+  mutate(rating = str_to_lower(rating),
+         candy = str_remove(candy, "^\\[|Q6 \\| "),
+         candy = str_remove(candy, "\\]$"),
+         candy = str_to_lower(candy))
 
 
 
 ##### 
-# SECTION 2: IMPORT AND CLEAN EACH DATA SET, THEN COMBINE THEM
+# SECTION 3: MAIN QUESTIONNAIRE WITH ANY OTHER JOY/DESPAIR STANDARDISED
+# Section 3 can be commented out to remove these additional joy/despair columns
 
-# import and extract relevant data from 2015 data file (file specific)
-candy_2015_import <- import_xlsx_and_clean_names("boing-boing-candy-2015.xlsx")
+# select required columns
+candy_2015_extra <- candy_2015 %>%
+  select(year:going_trick_or_treating,
+         joy = `Please list any items not included above that give you JOY.`,
+         despair = `Please list any items not included above that give you DESPAIR.`)
 
-candy_2015_col_clean <- candy_2015_import %>%
-  rename(age = how_old_are_you,
-         going_trick_or_treating = are_you_going_actually_going_trick_or_treating_yourself,
-         joy = please_list_any_items_not_included_above_that_give_you_joy,
-         despair = please_list_any_items_not_included_above_that_give_you_despair
-  ) %>% 
-  select(year:despair, necco_wafers)
+candy_2016_extra <- candy_2016 %>%
+  select(year:country,
+         joy = `Please list any items not included above that give you JOY.`,
+         despair = `Please list any items not included above that give you DESPAIR.`)
 
-candy_2015_main_list <- candy_2015_col_clean %>%
-  select(-joy, -despair) %>% 
-  pivot_longer(cols = butterfinger:last_col(),
-               names_to = "candy",
-               values_to = "rating"
-  ) %>% 
-  mutate(candy = str_replace_all(candy, "_", " ")) %>% 
-  filter(!is.na(rating))
+candy_2017_extra <- candy_2017 %>%
+  select(year:country,
+         joy = `Q7: JOY OTHER`,
+         despair = `Q8: DESPAIR OTHER`)
 
-candy_2015_other_items <- candy_2015_col_clean %>% 
-  select(year, rater_id, age,
-         going_trick_or_treating,
-         joy, despair
-  ) %>%
-  others_to_candy("joy", "despair")
+# combine all three data sets
+candy_extra_combined <- bind_rows(candy_2015_extra, candy_2016_extra,
+                                  candy_2017_extra) %>% 
+  # convert to long format
+  pivot_longer(cols = joy:despair,
+               names_to = "rating",
+               values_to = "candy") %>%
+  # split rows with multiple entries, seperated by commas or semi-colons
+  separate_longer_delim(cols = candy,
+                        delim = regex("[\\,\\;]")) %>%
+  filter(!is.na(candy) & candy != "") %>%
+  # remove leading and lagging spaces and convert to lower case
+  mutate(candy = trimws(str_to_lower(candy))) %>%
+  # remove any candies with less than 10 entries
+  group_by(candy) %>%
+  filter(n() > 9)
 
-candy_2015_data <- bind_rows(candy_2015_main_list,
-                             candy_2015_other_items)
-
-
-# import and extract relevant data from 2016 data file (file specific)
-candy_2016_import <- import_xlsx_and_clean_names("boing-boing-candy-2016.xlsx")
-
-candy_2016_col_clean <- candy_2016_import %>%
-  rename(age = how_old_are_you,
-         going_trick_or_treating = are_you_going_actually_going_trick_or_treating_yourself,
-         gender = your_gender,
-         country = which_country_do_you_live_in,
-         joy = please_list_any_items_not_included_above_that_give_you_joy,
-         despair = please_list_any_items_not_included_above_that_give_you_despair
-  ) %>% 
-  select(year:despair)
-
-candy_2016_main_list <- candy_2016_col_clean %>%
-  select(-joy, -despair) %>%
-  pivot_longer(cols = x100_grand_bar:last_col(),
-               names_to = "candy",
-               values_to = "rating"
-  ) %>% 
-  mutate(candy = str_replace_all(candy, "_", " ")) %>% 
-  filter(!is.na(rating))
-
-candy_2016_other_items <- candy_2016_col_clean %>% 
-  select(year, rater_id, age, gender,
-         going_trick_or_treating,
-         country,
-         joy, despair) %>% 
-  others_to_candy("joy", "despair")
-
-candy_2016_data <- bind_rows(candy_2016_main_list,
-                             candy_2016_other_items)
-
-
-# import and extract relevant data from 2017 data file (file specific)
-candy_2017_import <- import_xlsx_and_clean_names("boing-boing-candy-2017.xlsx")
-
-candy_2017_col_clean <- candy_2017_import %>%
-  rename_with(~str_remove(., "^q[1-6]_")) %>% 
-  rename(going_trick_or_treating = going_out,
-         joy = q7_joy_other,
-         despair = q8_despair_other) %>% 
-  select(year:despair)
-
-candy_2017_main_list <- candy_2017_col_clean %>%
-  select(-joy, -despair) %>% 
-  pivot_longer(cols = `100_grand_bar`:last_col(),
-               names_to = "candy",
-               values_to = "rating"
-  ) %>% 
-  mutate(candy = str_replace_all(candy, "_", " ")) %>% 
-  filter(!is.na(rating))
-
-candy_2017_other_items <- candy_2017_col_clean %>% 
-  select(year, rater_id, age, gender,
-         going_trick_or_treating,
-         country,
-         joy, despair) %>%
-  others_to_candy("joy", "despair")
-
-candy_2017_data <- bind_rows(candy_2017_main_list,
-                             candy_2017_other_items)
-
-
-# combine all 3 data sets and remove redundant columns
-all_candy_data_combined <- bind_rows(candy_2015_data,
-                                     candy_2016_data,
-                                     candy_2017_data)
-
-
-all_candy_data_combined <- all_candy_data_combined %>% 
-  select(year, rater_id, age, gender, country, 
-         going_trick_or_treating, candy, rating)
+# Join main questionnaire responses to other joy/despair responses
+candy_combined <- bind_rows(candy_combined,
+                            candy_extra_combined)
 
 
 
 #####
-# SECTION 3: DEEP CLEAN OF FULL DATA SET
+# SECTION 4: COMBINE SIMILAR NAMED CANDY AND CHECK VALIDITY
 
-# combine similar sounding candy names and only keep joy/meh/despair entries
-source(here::here("data_cleaning_scripts/lists/alt_entry_names_for_candy.R"))
-
-alternative_candy_names <- enframe(candy_list) %>%
+# create list to combine similar named candy
+candy_alt_names <- enframe(candy_list) %>%
   unnest_longer(col = value, values_to = "replace_this")
 
-alt_candy_combined <- all_candy_data_combined %>%
-  mutate(rating = str_to_lower(str_trim(rating)),
-         candy = str_to_lower(str_trim(candy))
-  ) %>%
-  filter(rating %in% c("joy", "despair", "meh")) %>%
-  left_join(alternative_candy_names, by = join_by(candy == replace_this)) %>%
+candy_renamed <- candy_combined %>% 
+  # combine similar sounding candy names
+  left_join(candy_alt_names, by = join_by(candy == replace_this)) %>%
   mutate(candy = if_else(!is.na(name), name, candy)) %>% 
-  select(-name)
+  select(-name) %>% 
+  # check whether candy name is a real candy
+  filter(candy %in% candy_names)
 
 
-# check candy names exists and remove those with less than 10 entries 
-source(here::here("data_cleaning_scripts/lists/candy_names.R"))
 
-fixed_candy_data <- alt_candy_combined %>% 
-  mutate(is_candy = (candy %in% candy_names)) %>%
-  filter(is_candy) %>%
-  group_by(candy) %>%
-  filter(n() > 9) %>%
-  ungroup() %>% 
-  select(-is_candy)
+#####
+# SECTION 5: SPLIT COUNTRIES INTO CANADA, UK, USA, ROW and NA
 
-
-# filter countries into Canada, UK, USA, others and NA
-source(here::here("data_cleaning_scripts/lists/alt_entry_names_for_countries.R"))
-
-alternative_country_names <- enframe(country_list) %>%
-  unnest_longer(col = value, values_to = "replace_this")
-
-candy_and_country_data <- fixed_candy_data %>% 
-  left_join(alternative_country_names, by = join_by(country == replace_this)) %>% 
-  mutate(name_2 = case_when(str_detect(country, "^(?i)canada") ~ "Canada",
-                            str_detect(country, "^(?i)unite[sd] st") ~ "United States of America",
-                            str_detect(country, "^(?i)U[\\ \\.]*S") ~ "United States of America",
-                            str_detect(country, "mer") ~ "United States of America",
-                            str_detect(country, "^(?i)U[\\ ]*K$") ~ "United Kingdom",
-                            str_detect(country, "^United Ki[:alpha:]*om$") ~ "United Kingdom",
-                            TRUE ~ NA),
-         .after = name
-  ) %>%
+country_candy_data <- candy_renamed %>%   
   mutate(country = case_when(is.na(country) ~ NA,
-                             is.numeric(country) ~ NA,
-                             !is.na(name_2) ~ name_2,
-                             !is.na(name) ~ name,
-                             TRUE ~ "Other")
-  ) %>% 
-  select(-name, -name_2)
+                             str_detect(country, "^[\\.[:digit:]]+$") ~ NA,
+                             # Rename as Canada
+                             str_detect(country, "^(?i)canada") ~ "Canada",
+                             # Rename as UK
+                             str_detect(country, "^(?i)U[\\ ]*K$") ~ "UK",
+                             str_detect(country, "^United Ki[:alpha:]*om$") ~ "UK",
+                             str_detect(country, "^(?i)[es][:alpha:]{2,3}land$") ~ "UK",
+                             # Rename as USA
+                             str_detect(country, "^(?i)U[\\ \\.]*S") ~ "USA",
+                             str_detect(country, "(?i)unit[esd]+ s") ~ "USA",
+                             str_detect(country, "((?i)m)er") ~ "USA",
+                             country %in% country_list$`United States of America` ~ "USA",
+                             # Rename as ROW
+                             TRUE ~ "ROW"))
 
 
-# update age list to be 1-100 years old, rest are NA
-age_candy_country_data <- candy_and_country_data %>% 
+
+#####
+# SECTION 6: UPDATE AGE LIST TO BE 4-100 years old; set rest to NA
+
+age_country_candy_data <- country_candy_data %>% 
   mutate(age_floor = floor(as.numeric(age)),
-         age = if_else(age_floor > 0 & age_floor <= 100,
+         age = if_else(age_floor > 3 & age_floor <= 100,
                        age_floor,
                        NA)) %>% 
   select(-age_floor)
@@ -211,8 +170,8 @@ age_candy_country_data <- candy_and_country_data %>%
 
 
 #####
-# SECTION 4: EXPORT CLEAN DATA AS CSV
-age_candy_country_data %>%
+# SECTION 7: EXPORT CLEAN DATA AS CSV
+age_country_candy_data %>%
   select(year, rater_id, age, gender, country, 
          going_trick_or_treating, candy, rating) %>% 
   write_csv(here::here("clean_data/candy_clean.csv"))
